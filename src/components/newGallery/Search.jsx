711fdd591c92b598d26422f372b948e3bdb11cdf
Theglxry <102861719+Theglxry@users.noreply.github.com>
2023-11-18 17:44:33 +0100

    refacturing

--- src/components/newGallery/Search.jsx
+++ src/components/newGallery/Search.jsx
@@ -34,21 +34,24 @@ function SearchBar({ onSearch }) {
   
   return (
     <div style={mainStyle} className="w-full">
-      <div className="relative p-12 w-full sm:max-w-2xl sm:mx-auto">
-        <div className="overflow-hidden z-0 rounded-full relative p-3">
+      <div className="relative p-8 sm:p-12 w-full sm:max-w-2xl sm:mx-auto ">
+        <div className="overflow-hidden z-0 rounded-full relative p-3 flex item-center justify-center ">
           <form
             role="form"
-            className="relative flex z-50 bg-white rounded-full"
+            className="relative flex z-50 bg-white rounded-full w-7/12 sm:w-full"
           >
             <input
               type="text"
               placeholder="search luxury cars "
-              className="rounded-full flex-1 px-6 py-4 text-gray-700 focus:outline-none"
+              className="rounded-full flex-1 px-4 text-[12px] sm:px-6 sm:py-4 text-gray-700 focus:outline-none"
               value={query}
               onChange={handleInputChange}
             />
+
+
+
             <button
-              className="bg-indigo-500 text-white rounded-full font-semibold px-8 py-4 hover:bg-indigo-400 focus:bg-indigo-600 focus:outline-none"
+              className="bg-indigo-500 text-white text-[10px] rounded-full font-semibold ml-[-3.5rem] py-2 px-2 sm:px-8 sm:py-4 hover:bg-indigo-400 focus:bg-indigo-600 focus:outline-none"
               onClick={handleSearchClick}
               disabled={!query}
             >
